2f8886f2d1e1cab8611525e1fc970cbcdf5f23dd
chintan-gigaprofits <chintan@gigaprofits.com>
2025-08-08 21:15:17 -0400

    Add project files.

---
+++ Bulky.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Bulky.DataAccess.Repository.IRepository
+{
+    public interface IUnitOfWork
+    {
+        ICategoryRepository Category { get; }
+        IProductRepository Product { get; }
+        void Save(); // this will save all the changes made in the repository
+    }
+}
